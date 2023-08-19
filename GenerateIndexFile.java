629d6a22834188dc77f4168d511551f360ed151c
Made By IToncek <toncek2019@post.cz>
2023-08-19 18:09:09 +0200

    Finalizing?

---
+++ GenerateIndexFile.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("./public");
+
+        if(!file.exists()) System.exit(404);
+        List<File> dirs = new ArrayList<>();
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            if(f.isDirectory()){
+                dirs.add(f);
+            }
+        }
+
+        File file1 = new File("./public/index.html");
+        File file2 = new File("./public/hipslist");
+        if(file1.exists()) file1.delete();
+        if(file2.exists()) file2.delete();
+
+        file1.createNewFile();
+        file2.createNewFile();
+
+        try (FileWriter indexWriter = new FileWriter(file1)) {
+            try (FileWriter hipsWriter = new FileWriter(file2)) {
+                indexWriter.write("""
+                        <head>
+                            <title>HiPS.itoncek.space</title>
+                        </head>
+                        <body>
+                            <h1>HiPS.itoncek.space</h1>""");
+                for (File dir : dirs) {
+                    indexWriter.write(String.format("    <a href=\"%s\">%s</a> <br>\n", "./" + dir.getName() + "/", "/" + dir.getName()));
+                    hipsWriter.write(String.format("hips_service_url = %s\n", "./" + dir.getName()));
+                    hipsWriter.write(String.format("hips_release_date = %s\n", Instant.now().toString()));
+                    hipsWriter.write("hips_status = public mirror clonableOnce\n");
+                }
+                indexWriter.write("</body>");
+            }
+        }
+    }
+}
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 mkdir public 
-cp hipslist public/hipslist
-cp index.html public/index.html
 wget -O aladin.jar https://aladin.cds.unistra.fr/java/Hipsgen.jar
 java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" id=HERE/p/danielkurtin color=png mode=overlayFading
 rm aladin.jar
+java GenerateIndexFile.java
