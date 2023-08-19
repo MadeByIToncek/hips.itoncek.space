8ce3743e586565442150de68c0d8375d44383662
Made By IToncek <toncek2019@post.cz>
2023-08-19 17:43:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir public 
 cp hipslist public/hipslist
 cp index.html public/index.html
-wget -O aladin.jar https://github.com/gohugoio/hugo/archive/master.zip
+wget -O aladin.jar https://aladin.cds.unistra.fr/java/Hipsgen.jar
 java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" id=HERE/p/danielkurtin color=png mode=overlayFading
 rm aladin.jar
