d1856d5db6f3a29d931c0e9e5e9b3312256b0357
Made By IToncek <toncek2019@post.cz>
2023-08-19 19:52:04 +0200

    Moving .java files to separate repo

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ java -Xmx6G -Djava.io.tmpdir=/cache/java -jar aladin.jar in="./danielkurtin" out
 java -Xmx6G -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" PNG
 java -Xmx6G -jar aladin.jar out="./public/danielkurtin" TRIM
 rm aladin.jar
+wget -O GenerateIndexFile.java https://github.com/MadeByIToncek/hipstools/raw/master/src/GenerateIndexFile.java
 javac GenerateIndexFile.java
 java GenerateIndexFile
+rm GenerateIndexFile.java
+rm GenerateIndexFile.class
