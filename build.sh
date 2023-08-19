481105d003306cd8ad6158c9928f1b1f7a4e221b
Made By IToncek <toncek2019@post.cz>
2023-08-19 18:47:02 +0200

    CI

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 mkdir public 
 wget -O aladin.jar https://aladin.cds.unistra.fr/java/Hipsgen.jar
-java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" id=HERE/p/danielkurtin color=png mode=overlayFading
+mkdir cache/aladin
+mkdir cache/java
+java -Xmx6G -Djava.io.tmpdir=/cache/java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" maxThread=20 cache="/cache/aladin" cacheRemoveOnExit=false id=HERE/p/danielkurtin color=png mode=overlayFading
+java -Xmx6G -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" PNG
+java -Xmx6G -jar aladin.jar out="./public/danielkurtin" CLEANFITS
+java -Xmx6G -jar aladin.jar out="./public/danielkurtin" TRIM
 rm aladin.jar
 java GenerateIndexFile.java
