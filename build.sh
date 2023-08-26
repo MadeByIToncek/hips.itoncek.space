3dd31fa37177523eee1b946b0a4441f3f82cb049
Made By IToncek <toncek2019@post.cz>
2023-08-26 19:29:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ wget -O aladin.jar https://aladin.cds.unistra.fr/java/Hipsgen.jar
 mkdir cache
 mkdir cache/aladin
 mkdir cache/java
-java -Xmx6G -Djava.io.tmpdir=./cache/java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" maxThread=20 cache="./cache/aladin" cacheRemoveOnExit=false order=7 id=HERE/p/danielkurtin color=jpeg mode=overlayFading
-java -Xmx6G -Djava.io.tmpdir=./cache/java -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" cache="./cache/aladin" cacheRemoveOnExit=false JPEG
+java -Xmx6G -Djava.io.tmpdir=./cache/java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" maxThread=20 cache="./cache/aladin" cacheRemoveOnExit=false order=6 id=HERE/p/danielkurtin color=png mode=overlayFading
+java -Xmx6G -Djava.io.tmpdir=./cache/java -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" cache="./cache/aladin" cacheRemoveOnExit=false PNG
 java -Xmx6G -Djava.io.tmpdir=./cache/java -jar aladin.jar out="./public/danielkurtin" cache="./cache/aladin" cacheRemoveOnExit=false TRIM
 rm aladin.jar
 wget -O GenerateIndexFile.java https://github.com/MadeByIToncek/hipstools/raw/master/src/GenerateIndexFile.java
@@ -12,3 +12,4 @@ javac GenerateIndexFile.java
 java GenerateIndexFile
 rm GenerateIndexFile.java
 rm GenerateIndexFile.class
+ls -la
