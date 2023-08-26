78cb5f0d3c2195b35b3216e8df0ad859f7f5bfc7
Made By IToncek <toncek2019@post.cz>
2023-08-26 18:59:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 mkdir public 
 wget -O aladin.jar https://aladin.cds.unistra.fr/java/Hipsgen.jar
-#mkdir cache
-#mkdir cache/aladin
-#mkdir cache/java
-#java -Xmx6G -Djava.io.tmpdir=/cache/java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" maxThread=20 cache="/cache/aladin" cacheRemoveOnExit=false id=HERE/p/danielkurtin color=jpeg mode=overlayFading
-#java -Xmx6G -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" JPEG
-#java -Xmx6G -jar aladin.jar out="./public/danielkurtin" TRIM
+mkdir cache
+mkdir cache/aladin
+mkdir cache/java
+java -Xmx6G -Djava.io.tmpdir=/cache/java -jar aladin.jar in="./danielkurtin" out="./public/danielkurtin" maxThread=20 cache="/cache/aladin" cacheRemoveOnExit=false id=HERE/p/danielkurtin color=jpeg mode=overlayFading
+java -Xmx6G -jar aladin.jar out="./public/danielkurtin" pixelCut="-8000 16000 sqrt" JPEG
+java -Xmx6G -jar aladin.jar out="./public/danielkurtin" TRIM
 rm aladin.jar
 wget -O GenerateIndexFile.java https://github.com/MadeByIToncek/hipstools/raw/master/src/GenerateIndexFile.java
 javac GenerateIndexFile.java
